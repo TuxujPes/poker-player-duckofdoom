013be42aac2f21de6d364f11b5bda8633644ae1e
Taras Kharuk <tuxuj.pes@yandex.ru>
2016-03-26 12:10:57 +0200

    check table pairs

--- player.js
+++ player.js
@@ -12,7 +12,7 @@ var bot = {
 
     try {
 
-      if (this.isPair()) {
+      if (this.isHandPair() || this.isTablePair()) {
         money = min_raise * 2;
       } else if (this.getOurCardSum() > 20) {
         money = min_raise;
@@ -47,6 +47,10 @@ var bot = {
     return this.getOurPlayer().hole_cards;
   },
 
+  getTableCards: function() {
+    return this.GS.community_cards;
+  },
+
   getOurCardSum: function() {
     var cards = this.getOurCards();
 
@@ -68,10 +72,32 @@ var bot = {
       : +card.rank;
   },
 
-  isPair: function() {
+  isHandPair: function() {
     var cards = this.getOurCards();
 
     return cards[0].rank === cards[1].rank;
+  },
+
+  isTablePair: function() {
+    var table = this.getTableCards();
+    var our = this.getOurCards();
+    var allcards = table.concat(our);
+    var hash = {};
+    var rank;
+
+    for (var i = 0; i < allcards.length; i++) {
+      rank = allcards[i].rank;
+
+      hash[rank] = hash[rank] ? hash[rank] + 1  : 1;
+    }
+
+    for (var key in hash) {
+      if (hash[key] > 1) {
+        return true;
+      }
+    }
+
+    return false;
   }
 };
 
