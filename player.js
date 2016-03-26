2473c1977fcce64cb44fe0abe2c208129302b826
Taras Kharuk <tuxuj.pes@yandex.ru>
2016-03-26 14:49:15 +0200

    try to fix...

--- player.js
+++ player.js
@@ -4,53 +4,33 @@ var bot = {
 
   VERSION: "Default JavaScript folding player",
 
-  MIN_SUM: 20,
-
-  bet_request: function(gs, makeBet) {
+  bet_request: function(gs, bet) {
     this.GS = gs;
 
+    var money = 0;
+    var min_raise = gs.minimum_raise + 2 * gs.small_blind;
+
     try {
-      bet = this.getBet();
+
+      if (this.isHandPair() || this.isTablePair()) {
+        money = min_raise * 2;
+      } else if (this.getOurCardSum() > 20) {
+        money = min_raise;
+      }
+
+
     }
     catch(err) {
       log('ERRORe', err);
     }
 
-    makeBet(makeBet);
-  },
+    log(money);
 
-  high_card_value: 11,
+    bet(money);
+  },
 
   showdown: function(gs) {
 
-  },
-
-  getBet: function() {
-    var bet = 0;
-    var gs = this.GS;
-    var min_raise = gs.minimum_raise + 2 * gs.small_blind;
-
-    if (this.isPreFlop()) {
-      // PRE FLOP
-      if (this.isHandPair()) {
-        bet = min_raise * 2;
-      } else if (this.getOurCardSum() > this.MIN_SUM) {
-        bet = min_raise;
-      }
-    } else {
-      // POST FLOP
-      if (this.isHandPair() || this.isTablePairOrMore()) {
-        if (this.isSet() || this.isFlush()){
-          bet = this.getOurPlayer().stack;
-        } else {
-          bet = min_raise * 2;
-        }
-      } else if (this.getOurCardSum() > 20) {
-        bet = min_raise;
-      }
-    }
-
-    return bet;
   },
 
   getOurPlayer: function() {
@@ -98,89 +78,7 @@ var bot = {
     return cards[0].rank === cards[1].rank;
   },
 
-  isTablePairOrMore: function() {
-    var hash = this.getCardsHash();
-    for (var key in hash) {
-      if (hash[key] > 1) {
-        return true;
-      }
-    }
-
-    return false;
-  },
-  isPairWorthPlaying: function() {
-    var hash = this.gerCardsHash(),
-        pairedCards = [],
-        countOfStrongerCards = 0;
-    for (var key in hash) {
-      if (hash[key] == 2) {
-        pairedCards.push(this.getCardValue(hash[key]));
-      }
-    }
-    if (pairedCards.length > 1) {
-      //two pairs worth playing
-      return true;
-    }
-    if (pairedCards[0] >= this.high_card_value){
-      //is a high pair
-      return true;
-    } else {
-      for (var key in hash) {
-        if (pairedCards.indexOf(this.getCardValue(key)) == -1) {
-          if (this.getCardValue(key) > pairedCards[0]){
-            countOfStrongerCards++;
-          }
-        }
-      }
-      return !(countOfStrongerCards > 1);
-    }
-  },
-  isSet: function() {
-    var hash = this.getCardsHash();
-    for (var key in hash) {
-      if (hash[key] == 3) {
-        return true;
-      }
-      return false;
-    }
-
-  },
-
-  isFlush: function() {
-    var hash = this.getSuitsHash();
-    for (var key in hash) {
-      if (hash[key] === 5) {
-        return true;
-      }
-    }
-    return false;
-  },
-
-  isPreFlop: function() {
-    return this.GS.community_cards.length === 0;
-  },
-
-  isFlop: function() {
-    return this.GS.community_cards.length === 3;
-  },
-
-  getSuitsHash: function() {
-    var table = this.getTableCards();
-    var our = this.getOurCards();
-    var allcards = table.concat(our);
-    var hash = {};
-    var suit;
-
-    for (var i = 0; i < allcards.length; i++) {
-      suit = allcards[i].suit;
-
-      hash[suit] = hash[suit] ? hash[suit] + 1  : 1;
-    }
-
-    return hash;
-  },
-
-  getCardsHash: function() {
+  isTablePair: function() {
     var table = this.getTableCards();
     var our = this.getOurCards();
     var allcards = table.concat(our);
@@ -193,7 +91,13 @@ var bot = {
       hash[rank] = hash[rank] ? hash[rank] + 1  : 1;
     }
 
-    return hash;
+    for (var key in hash) {
+      if (hash[key] > 1) {
+        return true;
+      }
+    }
+
+    return false;
   }
 };
 
