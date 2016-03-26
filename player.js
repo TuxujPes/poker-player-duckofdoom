047f2192c0ffa96be33281926da861c5b8222863
Taras Kharuk <tuxuj.pes@yandex.ru>
2016-03-26 10:53:06 +0200

    bet index

--- player.js
+++ player.js
@@ -4,7 +4,7 @@ module.exports = {
   VERSION: "Default JavaScript folding player",
 
   bet_request: function(game_state, bet) {
-    bet(300);
+    bet(game_state.minimum_raise + (50 * (game_state.bet_index + 1)));
   },
 
   showdown: function(game_state) {
