9692f16553101e28bb523ac4027ef69f74b1c2c5
Taras Kharuk <tuxuj.pes@yandex.ru>
2016-03-26 11:11:43 +0200

    tries to sum

--- player.js
+++ player.js
@@ -5,14 +5,58 @@ module.exports = {
 
   VERSION: "Default JavaScript folding player",
 
-  bet_request: function(game_state, bet) {
-    const bet = game_state.minimum_raise + (50 * (game_state.bet_index + 1));
+  bet_request: function(gs, bet) {
+    this.GS = gs;
+
+    let bet = 0;
+
+    try {
+      if (this.getOurCardSum() > 20) {
+        bet = gs.minimum_raise + 2 * gs.small_blind);
+      }
+    }
+
     log(bet);
 
     bet(bet);
   },
 
-  showdown: function(game_state) {
+  showdown: function(gs) {
 
+  },
+
+  getOurPlayer: function() {
+    const players = this.GS.players;
+
+    for (var i = 0; i < players.length; i++) {
+      if (players[i].hole_cards) {
+        return players[i];
+      }
+    }
+  },
+
+  getOurCards: function(player) {
+    return this.getOurPlayer().hole_cards;
+  },
+
+  getOurCardSum: function() {
+    const cards = this.getOurCards();
+
+    return cards.reduce(function(memo, card) {
+      return memo + this.getCardValue(card);
+    }.bind(this), 0);
+  },
+
+  getCardValue: function(card) {
+    const high = {
+      'J': 11,
+      'Q': 12,
+      'K': 13,
+      'A': 14
+    };
+
+    return card.rank in high
+      ? high[card.rank]
+      : +card.rank;
   }
 };
