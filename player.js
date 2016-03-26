6e32c706d77fc373037e75e846e319d67222c1da
Taras Kharuk <tuxuj.pes@yandex.ru>
2016-03-26 10:55:22 +0200

    log once

--- player.js
+++ player.js
@@ -1,10 +1,15 @@
+const log = console.log.bind(console);
+
 
 module.exports = {
 
   VERSION: "Default JavaScript folding player",
 
   bet_request: function(game_state, bet) {
-    bet(game_state.minimum_raise + (50 * (game_state.bet_index + 1)));
+    const bet = game_state.minimum_raise + (50 * (game_state.bet_index + 1));
+    log(bet);
+
+    bet(bet);
   },
 
   showdown: function(game_state) {
